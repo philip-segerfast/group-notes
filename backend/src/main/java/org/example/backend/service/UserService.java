dc2421458aedfe232c57392db32f728071e7f782
Robin Persson <rp.ullstorp92@gmail.com>
2024-04-27 17:16:02 +0200

    clean up

--- backend/src/main/java/org/example/backend/service/UserService.java
+++ backend/src/main/java/org/example/backend/service/UserService.java
@@ -1,19 +1,14 @@
 package org.example.backend.service;
 
 import lombok.RequiredArgsConstructor;
-import org.example.backend.api.modules.groups.GroupResponse;
-import org.example.backend.api.modules.groups.StoredGroups;
 import org.example.backend.api.modules.groups.user.CreateUserRequest;
 import org.example.backend.api.modules.groups.user.GetUserResponse;
 import org.example.backend.api.modules.groups.user.StoredUsersResponse;
-import org.example.backend.entity.User;
 import org.example.backend.repository.UserRepository;
 import org.example.backend.service.converter.UserConverter;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
