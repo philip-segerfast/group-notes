365ce375b402bb295db998849675b3f8020028fc
Philip <psegerfast@gmail.com>
2024-06-09 23:09:50 +0200

    Update build.gradle.kts

--- server/spring/build.gradle.kts
+++ server/spring/build.gradle.kts
@@ -5,11 +5,15 @@ plugins {
     application
 }
 
-group = "org.robphi.groupnotes"
+group = "org.example"
 version = "1.0.0"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
 application {
-    mainClass.set("org.example.spring.BackendApplication")
+    mainClass.set("org.example.backend.BackendApplication")
 }
 
 configurations {
@@ -18,23 +22,29 @@ configurations {
     }
 }
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
     implementation(projects.shared)
     implementation(projects.server.shared)
 
-    implementation(libs.logback)
     testImplementation(libs.kotlin.test.junit)
 
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
     implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.0.4")
 
-    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    runtimeOnly("org.mariadb:r2dbc-mariadb:1.2.0")
+
+    annotationProcessor("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok")
 }
